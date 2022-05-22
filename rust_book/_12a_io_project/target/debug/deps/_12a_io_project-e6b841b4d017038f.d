eb4bec6a7691afbbc859595ee77bb1ebcb0d1ccd
DESKTOP-B09ANSL\Quang <chuvanquang0104@gmail.com>
2022-05-22 23:28:13 +0700

    new book

---
+++ rust_book/_12a_io_project/target/debug/deps/_12a_io_project-e6b841b4d017038f.d
@@ -0,0 +1,5 @@
+D:\STUDY\RUST_STUDY\rust_beginner\rust_book\_12a_io_project\target\debug\deps\_12a_io_project-e6b841b4d017038f.exe: src\lib.rs
+
+D:\STUDY\RUST_STUDY\rust_beginner\rust_book\_12a_io_project\target\debug\deps\_12a_io_project-e6b841b4d017038f.d: src\lib.rs
+
+src\lib.rs:
