2c4f426ee3ebb885182eaff6d065e930e9401ade
DAO NGOC <ngocdb@acbs.com.vn>
2022-06-03 14:51:08 +0700

    update

---
+++ rust_book/_15b_defer/src/main.rs
@@ -0,0 +1,8 @@
+mod _1_regular_defer;
+mod _2_box_defer;
+mod _3_own_smart_pointer;
+mod _4_implicit_defer_coercions;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ rust_book/_15b_defer/target/debug/deps/_15b_defer-d351970d2626a56d.d
@@ -0,0 +1,7 @@
+C:\Users\ACBS\Desktop\clone\Introduction-to-Rust-Programming-\rust_book\_15b_defer\target\debug\deps\_15b_defer-d351970d2626a56d.exe: src\main.rs src\_1_regular_defer.rs src\_2_box_defer.rs
+
+C:\Users\ACBS\Desktop\clone\Introduction-to-Rust-Programming-\rust_book\_15b_defer\target\debug\deps\_15b_defer-d351970d2626a56d.d: src\main.rs src\_1_regular_defer.rs src\_2_box_defer.rs
+
+src\main.rs:
+src\_1_regular_defer.rs:
+src\_2_box_defer.rs:
